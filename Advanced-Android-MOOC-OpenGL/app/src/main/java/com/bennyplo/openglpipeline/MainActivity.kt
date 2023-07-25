977f0934b399cd8e9d28440597d36b13c0228ce5
Hùng Quốc Lý <hungly@chotot.vn>
2023-07-25 17:39:17 +0700

    Setup OpenGL project

---
+++ Advanced-Android-MOOC-OpenGL/app/src/main/java/com/bennyplo/openglpipeline/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.bennyplo.openglpipeline
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private var glView: MyView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main);
+        glView = MyView(this)
+//        setContentView(R.layout.activity_fullscreen);
+        setContentView(glView)
+        // Set full screen
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        glView?.let {
+            WindowInsetsControllerCompat(window, it)
+        }?.let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+}
